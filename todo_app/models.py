7fd868c03f8b8ea2584a6458e92eb991a0eea6a0
gautam <gautam_32b7@hotmail.com>
2025-11-12 19:17:47 +0530

    hash_password

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,11 +1,28 @@
 from database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 
 
+# SQLAlchemy model representing user in the database
+class Users(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String(50), unique=True)
+    username = Column(String(50), unique=True)
+    first_name = Column(String(50))
+    last_name = Column(String(50))
+    hashed_password = Column(String(255))
+    is_active = Column(Boolean, default=True)
+    role = Column(String(50))
+
+
+# SQLAlchemy model representing a todo item in the database
 class Todos(Base):
     __tablename__ = 'todos'
+
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey('users.id'))
--- todo_app/routers/auth.py
+++ todo_app/routers/auth.py
@@ -1,8 +1,51 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
+from starlette import status
+from passlib.context import CryptContext
+from typing import Annotated
+from sqlalchemy.orm import Session
+
+from database import session_local
+from models import Users
 
 router = APIRouter()
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
 
-@router.get('/auth/')
-async def get_user():
-    return 'authenticate'
+# Provide a database session and ensures it's closed after use
+def get_session():
+    session = session_local()
+    try:
+        yield session
+    finally:
+        session.close()
+
+
+# Defines a dependency that provides a database session to routes
+session_dep = Annotated[Session, Depends(get_session)]
+
+
+# Pydantic model for validating user data in requests
+class CreateUserRequest(BaseModel):
+    useranme: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
+
+
+# Create a new user in the database
+@router.post('/auth', status_code=status.HTTP_201_CREATED)
+async def create_user(session: session_dep, create_user_request: CreateUserRequest):
+    create_user_model = Users(
+        email=create_user_request.email,
+        username=create_user_request.useranme,
+        first_name=create_user_request.first_name,
+        last_name=create_user_request.last_name,
+        role=create_user_request.role,
+        hashed_password=bcrypt_context.hash(create_user_request.password),
+        is_active=True
+    )
+    session.add(create_user_model)
+    session.commit()
